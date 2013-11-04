1ed3abedf57b88d30ebc64d7f0bd79924e53571c
Sam Willis <sam.willis@event.ly>
2013-11-04 10:00:56 +0000

    Initial commit
    - add slides
    - set up code base

--- source/javascripts/lib/site.js
+++ source/javascripts/lib/site.js
@@ -2,30 +2,10 @@
 
 // Page load state
 
-$(document).ready(function() {
-  $('.splitter').hide();
-  // $('#homepage, nav').hide();
-});
-
-
-// Load homepage
-
-$(window).load(function() {
-  $('#homepage').fadeIn(3000);
-});
-
-
-// Engage splitter
-
-$('body').on('click', '.open-split', function() {
-  $('#homepage').fadeOut(200);
-  $('nav').delay(200).slideUp(300);
-  $('.splitter').delay(500).fadeIn(500);
-  $('body').removeClass("glass").delay(500).queue(function(next){
-    $(this).addClass("glass");
-    next();
-});
+$(function() {
+  $('.slide').css({'height': (($(window).height()) )});
 })
 
-
-// show splitter descriptions
+$(window).bind('resize', function(){
+  $('.slide').css({'height': (($(window).height()) )});
+});
