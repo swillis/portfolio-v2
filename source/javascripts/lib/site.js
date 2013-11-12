9d2c22e16d9091130a63e036dc6e86dc967f4a0b
Sam Willis <sam.willis@event.ly>
2013-11-12 18:18:44 +0000

    Edits n stuff. Don't judge me on my commit messages.

--- source/javascripts/lib/site.js
+++ source/javascripts/lib/site.js
@@ -6,6 +6,44 @@ $(function() {
   $('.slide').css({'height': (($(window).height()) )});
 })
 
-$(window).bind('resize', function(){
+$(window).resize(function(){
   $('.slide').css({'height': (($(window).height()) )});
 });
+
+$(function() {
+  var headHeight = $('.centered').height()/2;
+  var windowHeight = $(window).height();
+  var halfWay = windowHeight/2;
+  var navHeight = $('nav').height();
+  $('.centered').css({
+    'padding-top': halfWay - headHeight - navHeight
+  })
+})
+
+$(window).resize(function() {
+  var headHeight = $('.centered').height()/2;
+  var windowHeight = $(window).height();
+  var halfWay = windowHeight/2;
+  var navHeight = $('nav').height();
+  $('.centered').css({
+    'padding-top': halfWay - headHeight - navHeight
+  })
+})
+
+$(function() {
+  var headHeight = $('.bio').height()/2;
+  var windowHeight = $(window).height();
+  var halfWay = windowHeight/2;
+  $('.bio').css({
+    'padding-top': halfWay - headHeight - 100
+  })
+})
+
+$(window).resize(function() {
+  var headHeight = $('.bio').height()/2;
+  var windowHeight = $(window).height();
+  var halfWay = windowHeight/2;
+  $('.bio').css({
+    'padding-top': halfWay - headHeight - 100
+  })
+})
