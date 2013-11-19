21f1ca70b43d2155cb103bf8d8bbdcf43be6e7b1
Sam Willis <sam.willis@event.ly>
2013-11-19 12:00:57 +0000

    Add easter egg, remove erroneous bis and improve copy
    Also implemented feedback from friends and such.

--- source/javascripts/lib/site.js
+++ source/javascripts/lib/site.js
@@ -77,3 +77,10 @@ $(window).load(function() {
   $('nav').animate({'opacity':'1'},700, 'easeOutQuad');
   $('.headline').delay(700).animate({'opacity':'1'}, 700, 'easeOutQuad');
 })
+
+
+// hint
+
+$('body').on('click', '.title', function() {
+  alert('Up, up, down, down, left, right, left, right, B, A.');
+})
